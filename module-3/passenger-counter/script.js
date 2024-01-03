701aed35c0298acbd9e06a021224c0ac46ba303f
Alokananda Y <plantsfortrees@gmail.com>
2024-01-03 19:50:40 +0530

    Completed the passenger counter app

--- module-3/passenger-counter/script.js
+++ module-3/passenger-counter/script.js
@@ -1,12 +1,28 @@
 let count = 0;
-let saveEl = document.getElementById("save-el");
-let countEl = document.getElementById("count-el");
+const saveEl = document.getElementById("save-el");
+const countEl = document.getElementById("count-el");
+const saveBtnEl = document.getElementById("save-btn");
+const incrementBtn = document.getElementById("increment-btn");
 
 // Define the callback function
 const increment = () => {
-  console.log('Button clicked!');
-  // Add your logic or code here
+  count++;
+  countEl.textContent = count;
 };
 
- // Add a click event listener to the button and associate it with the callback function
- countEl.addEventListener('click', increment);
+// Add a click event listener to the "INCREMENT" button and associate it with the callback function
+incrementBtn.addEventListener('click', increment);
+
+// Update the entry upon save
+const saveAndUpdate = () => {
+  // Update the innerText with the current count
+  saveEl.innerText = 'Previous entry: ' + count;
+  
+  // Reset count to 0 after saving
+  count = 0;
+  
+  // Update the displayed count
+  countEl.innerText = count;
+}
+
+saveBtnEl.addEventListener('click', saveAndUpdate);
