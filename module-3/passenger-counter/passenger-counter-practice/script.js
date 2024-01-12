fbcdbbf75e7ea619aac83e1ddd3adfda5129d9cc
Alokananda Y <plantsfortrees@gmail.com>
2024-01-12 10:59:45 +0530

    Completed the app

--- module-3/passenger-counter/passenger-counter-practice/script.js
+++ module-3/passenger-counter/passenger-counter-practice/script.js
@@ -5,13 +5,16 @@ let countEl = document.getElementById('count-el');
 let saveEl = document.getElementById('save-el');
 let totalEl = document.getElementById('total-el');
 let passengerCount = 0;
+let totalEntries = 0;
 let previousEntries = [];
 
 // Increment
 const incrementPassengerCount = () => {
     countEl.textContent = "";
     passengerCount++;
+    totalEntries = calculateTotal(); // Update totalEntries with the current entry + previous entries
     countEl.textContent = passengerCount;
+    updateTotalCount();
 }
 
 incrementBtnEl.addEventListener('click', incrementPassengerCount);
@@ -20,6 +23,8 @@ incrementBtnEl.addEventListener('click', incrementPassengerCount);
 const savePassengerCount = () => {
     previousEntries.push(passengerCount);
     saveEl.textContent = "Previous entries: " + previousEntries.join(' - ');
+    totalEntries = calculateTotal(); // Update totalEntries with the current entry + previous entries
+    updateTotalCount();
 }
 
 saveBtnEl.addEventListener('click', savePassengerCount);
@@ -27,10 +32,22 @@ saveBtnEl.addEventListener('click', savePassengerCount);
 // Reset
 const resetPassengerCount = () => {
     countEl.textContent = "";
-    passengerCount = 0
-    countEl.textContent = passengerCount;
+    passengerCount = 0;
+    totalEntries = 0;
     previousEntries = [];
     saveEl.textContent = "Previous entries: ";
+    totalEl.textContent = "Total entry: " + totalEntries;
+    countEl.textContent = passengerCount;
 }
 
 resetBtnEl.addEventListener('click', resetPassengerCount);
+
+// Update total count
+const updateTotalCount = () => {
+    totalEl.textContent = "Total entry: " + totalEntries;
+}
+
+// Calculate total entry
+const calculateTotal = () => {
+    return passengerCount + previousEntries.reduce((acc, entry) => acc + entry, 0);
+}
