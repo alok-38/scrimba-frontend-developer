77c15ab8169de6eaa5d22217ce4c9be8e5c60ba5
Alokananda Y <plantsfortrees@gmail.com>
2024-01-22 09:48:56 +0530

    lead tracker version2

--- module-3/chrome-extention/lead-tracker-verion2/app.js
+++ module-3/chrome-extention/lead-tracker-verion2/app.js
@@ -1,21 +1,39 @@
 myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const unorderedListEl = document.createElement('ul');
 
 const promptOnHover = () => {
-    // Display a message when the user hovers over the input field
-    inputEl.addEventListener('mouseenter', () => {
+    inputEl.addEventListener('mouseover', () => {
         inputEl.style.fontWeight = 'normal';
         inputEl.value = 'Enter your leads';
     });
-    // Remove the message when the user leaves the input field
-    inputEl.addEventListener("mouseleave", () => {
-        inputEl.value = "";
-    });
-    // Remove the message when the user starts typing
-    inputEl.addEventListener("click", () => {
-        inputEl.value = "";
+
+    inputEl.addEventListener('click', () => {
+        inputEl.value = '';
     });
 }
 
-promptOnHover();
+const acceptUserInput = () => {
+    const userInput = inputEl.value.trim();
+    if (userInput !== '') {
+        myLeads.push(userInput);
+        displayLeads();
+        inputEl.value = '';
+    }
+}
+
+const displayLeads = () => {
+    unorderedListEl.innerHTML = '';
+
+    myLeads.forEach((lead) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = lead;
+        unorderedListEl.appendChild(listItem);
+    });
+
+    document.body.appendChild(unorderedListEl);
+}
+
+inputBtn.addEventListener('click', acceptUserInput);
+promptOnHover(); // Add this line to ensure the promptOnHover is initialized
