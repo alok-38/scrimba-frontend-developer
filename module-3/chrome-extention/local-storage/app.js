b0de672d8c4edc7c39c751a1e9da16fca8323e2b
Alokananda Y <plantsfortrees@gmail.com>
2024-01-23 06:31:18 +0530

    Local storage

--- module-3/chrome-extention/local-storage/app.js
+++ module-3/chrome-extention/local-storage/app.js
@@ -1,17 +1,9 @@
 const inputElement = document.getElementById('input-el');
-const ulElement = document.createElement('ul');
 const saveButtonElement = document.getElementById('input-btn');
-const resetButtonElement = document.getElementById('reset-btn');
+const ulElement = document.createElement('ul');
 
-const displayMessageOnHover = () => {
-    // Adding an event listener for the "mouseover" event
-    inputElement.addEventListener('mouseover', () => {
-        inputElement.value = 'Enter your leads';
-    });
-    // Remove display as soon as the mouse leaves
-    inputElement.addEventListener('mouseleave', () => {
-        inputElement.value = '';
-    });
-}
+let myLeads = [];
 
-displayMessageOnHover();
+
+
+   
