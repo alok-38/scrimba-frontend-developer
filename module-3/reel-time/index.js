692660a66db3414ab612b5841c59e2fd9554f0d5
Alokananda Y <plantsfortrees@gmail.com>
2024-01-28 12:10:19 +0530

    Tested Firebase config

--- module-3/reel-time/index.js
+++ module-3/reel-time/index.js
@@ -1,18 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 
 const appSettings = {
-    databaseURL: "https://playground-c5b18-default-rtdb.europe-west1.firebasedatabase.app"
+    databaseURL: "https://shopping-cart-f6e7d-default-rtdb.asia-southeast1.firebasedatabase.app"
 }
 
-const app = initializeApp(appSettings)
+const app = initializeApp(appSettings);
 
-console.log(app)
+const inputFieldEl = document.getElementById('input-field');
+const addButtonEl = document.getElementById('add-button');
 
-const inputFieldEl = document.getElementById("input-field")
-const addButtonEl = document.getElementById("add-button")
+const testFunction = () => {
+    console.log(app);
+}
 
-addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
-    console.log(`${inputValue} added to database`)
-})
+addButtonEl.addEventListener('click', testFunction);
