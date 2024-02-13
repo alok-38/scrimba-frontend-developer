0cdab7e43b3dbb6437a630f916ac259c234f3954
Alokananda Y <plantsfortrees@gmail.com>
2024-02-13 14:53:40 +0530

    reel time

--- module-3/mobile-app/reel-time/app.js
+++ module-3/mobile-app/reel-time/app.js
@@ -1,9 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-abc66-default-rtdb.asia-southeast1.firebasedatabase.app"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const moviesInDB = ref(database, "movies");
 const inputFieldEl = document.getElementById('input-field');
 const addButtonEl = document.getElementById('add-button');
-const containerEl = document.getElementsByClassName('container');
 
 const addToDatabase = () => {
     let inputValue = inputFieldEl.value;
+    push(moviesInDB, inputValue);
     const paragraphEl = document.createElement('p');
     paragraphEl.textContent = `${inputValue} added to database`;
     document.body.appendChild(paragraphEl);
