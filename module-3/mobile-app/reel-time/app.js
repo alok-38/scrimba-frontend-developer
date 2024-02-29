75fe8e3031e2fe25b666f53796eb3fe544e337ff
Alokananda Y <plantsfortrees@gmail.com>
2024-02-29 19:21:45 +0530

    movies

--- module-3/mobile-app/reel-time/app.js
+++ module-3/mobile-app/reel-time/app.js
@@ -1,13 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const appSettings = {
+    databaseURL: "https://playground-cbcd5-default-rtdb.asia-southeast1.firebasedatabase.app"
+}
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const moviesInDB = ref(database, "movies");
 const inputEl = document.getElementById('input-field');
 const buttonEl = document.getElementById('add-button');
 
 const addMovieToTheDatabase = () => {
-    let movie = inputEl.value;
-    const paraEl = document.createElement('p');
-    const movieTextNode = document.createTextNode(movie);
+    let movieInput = inputEl.value;
+    // Create a paragraph element
+    let paraEl = document.createElement('p');
+    // Create a text node with the message
+    let movieTextNode = document.createTextNode(`Added ${movieInput} to the database`);
+    // Append the text node to the paragraph element
     paraEl.appendChild(movieTextNode);
-    document.body.appendChild(paraEl);
+    // Append the paragraph element to the document body (or any other parent element)
     paraEl.style.textAlign = 'center';
+    document.body.appendChild(paraEl);
+    push(moviesInDB, movieInput);
+    // Clear the input field after adding the movie
     inputEl.value = '';
 }
 
