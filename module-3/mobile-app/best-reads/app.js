26121f694f90aa90c63c221359284c844b4c1732
Alokananda Y <plantsfortrees@gmail.com>
2024-03-01 08:21:32 +0530

    books

--- module-3/mobile-app/best-reads/app.js
+++ module-3/mobile-app/best-reads/app.js
@@ -1,28 +1,33 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
     databaseURL: "https://playground-cbcd5-default-rtdb.asia-southeast1.firebasedatabase.app"
-}
+};
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const booksInDB = ref(database, "books");
 
 const booksEl = document.getElementById("books");
+let ulEl = document.createElement('ul');
+booksEl.appendChild(ulEl); // Append the ul outside the loop
 
 onValue(booksInDB, function (snapshot) {
     let booksArray = Object.values(snapshot.val());
-    booksArray.forEach(book => {
-        console.log(book);
-    });
-})
+    // Challenge: Write a for loop where you console log each book.
+    for (let i = 0; i < booksArray.length; i++) {
+        let currentBook = booksArray[i]
+
+        // Challenge: Use the appendBookToBooksListEl() function to append book instead of console logging
+        appendBookToBooksListEl(currentBook)
+    }
+});
 
 const clearBooksListEl = () => {
     booksEl.innerHTML = '';
-}
+};
 
 const appendBookToBooksListEl = (bookValue) => {
     booksEl.innerHTML += `<li>${bookValue}</li>`;
-}
-
+};
