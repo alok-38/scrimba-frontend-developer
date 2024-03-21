7572b5f007726fda3af73303968711a1b53373da
Alokananda Y <plantsfortrees@gmail.com>
2024-03-21 16:59:28 +0530

    custom

---
+++ starting-react/functional-component/page-component/src/App.jsx
@@ -0,0 +1,19 @@
+export default function Page() {
+  return (
+    <div className="bg-slate-50 container mx-auto max-w-2xl p-10">
+      <h1 className="mb-10 text-center text-2xl">Reasons to be Excited about Learning React</h1>
+      <ol className="leading-8">
+        <li>React is a popular and in-demand library used for building dynamic user interfaces.</li>
+        <li>It allows for the creation of reusable UI components, making development more efficient.</li>
+        <li>React's virtual DOM offers improved performance by minimizing DOM manipulation.</li>
+        <li>It has a large and active community, providing ample resources and support for learners.</li>
+        <li>React's component-based architecture encourages a clean and modular codebase.</li>
+        <li>It offers great flexibility and can be easily integrated with other libraries and frameworks.</li>
+        <li>React's declarative syntax leads to more predictable code and easier debugging.</li>
+        <li>Learning React opens up opportunities for career advancement in web development.</li>
+        <li>It is backed by Facebook, ensuring ongoing development and updates.</li>
+        <li>React Native allows for the development of cross-platform mobile applications using React.</li>
+      </ol>
+    </div>
+  )
+}
