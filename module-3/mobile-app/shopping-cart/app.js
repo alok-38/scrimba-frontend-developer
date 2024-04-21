2b48ff8af2093191bc8bcfa8edd9892b5a87e9c0
Alokananda Y <plantsfortrees@gmail.com>
2024-04-21 17:17:56 +0530

    db

--- module-3/mobile-app/shopping-cart/app.js
+++ module-3/mobile-app/shopping-cart/app.js
@@ -1,14 +1,50 @@
-// Get elements
-const inputEl = document.getElementById('input-field');
-const buttonEl = document.getElementById('add-button');
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
-// Function to get input value and render
-const getInputAndRenderOutput = () => {
-    const inputValue = inputEl.value;
-    const paraEl = document.createElement('p');
-    paraEl.textContent = inputValue;
-    document.body.appendChild(paraEl);
-    inputEl.value = '';
+const appSettings = {
+  databaseURL:
+    "https://playground-f425e-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
+
+const inputFieldEl = document.getElementById("input-field");
+const addButtonEl = document.getElementById("add-button");
+const shoppingListEl = document.getElementById("shopping-list");
+
+addButtonEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+
+  push(shoppingListInDB, inputValue);
+
+  clearInputFieldEl();
+
+  appendItemToShoppingListEl(inputValue);
+});
+
+// Add event listener for changes in the shopping list data
+onValue(shoppingListInDB, (snapshot) => {
+  // Clear existing list items
+  shoppingListEl.innerHTML = "";
+  // Loop through each item in the snapshot
+  snapshot.forEach((childSnapshot) => {
+    // Get item value and append it to the list
+    const itemValue = childSnapshot.val();
+    appendItemToShoppingListEl(itemValue);
+  });
+});
+
+function clearInputFieldEl() {
+  inputFieldEl.value = "";
 }
 
-buttonEl.addEventListener('click', getInputAndRenderOutput);
+function appendItemToShoppingListEl(itemValue) {
+  shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+}
