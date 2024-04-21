9b8bba328a962f64deede5809e9539ea32007bb6
Alokananda Y <plantsfortrees@gmail.com>
2024-04-21 14:51:38 +0530

    bug fix

--- module-3/mobile-app/app.js
+++ module-3/mobile-app/app.js
@@ -2,12 +2,15 @@ const inputElement = document.getElementById("input-field");
 const buttonElement = document.getElementById("add-button");
 const ulElement = document.getElementById("shopping-list");
 
-const getInputFromTheUser = () => {
-  const getInput = inputElement.value;
-  const liElement = document.createElement("li");
-  const textNode = document.createTextNode(getInput);
-  liElement.appendChild(textNode);
-  ulElement.appendChild(liElement);
+const handleAddToCart = () => {
+  const getInput = inputElement.value.trim(); // Trim to remove leading/trailing whitespace
+  if (getInput !== "") {
+    const liElement = document.createElement("li");
+    const textNode = document.createTextNode(getInput);
+    liElement.appendChild(textNode);
+    ulElement.appendChild(liElement);
+    inputElement.value = ""; // Clear the input field after adding item
+  }
 };
 
-buttonElement.addEventListener("click", getInputFromTheUser);
+buttonElement.addEventListener("click", handleAddToCart);
