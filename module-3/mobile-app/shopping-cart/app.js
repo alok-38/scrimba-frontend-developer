179eb8697b67f2cd20f38e8a1ef70a080dd5876d
Alokananda Y <plantsfortrees@gmail.com>
2024-04-22 09:10:29 +0530

    cart

--- module-3/mobile-app/shopping-cart/app.js
+++ module-3/mobile-app/shopping-cart/app.js
@@ -1,7 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://shopping-cart-fd6ad-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
 const buttonElement = document.getElementById("add-button");
 const inputElement = document.getElementById("input-field");
-const formElement = document.querySelector("form");
 const ulElement = document.querySelector(".food--items");
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const shoppingListInDB = ref(database, "shoppingList");
 
 const logFoodItem = (event) => {
   event.preventDefault();
@@ -12,6 +26,7 @@ const logFoodItem = (event) => {
   liElement.appendChild(textNode);
   //   Append this to the ul
   ulElement.appendChild(liElement);
+  push(shoppingListInDB, foodItem);
   inputElement.value = "";
 };
 
