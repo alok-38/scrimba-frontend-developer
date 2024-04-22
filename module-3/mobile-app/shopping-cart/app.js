bbee74c7433397879c195830c2d4f5e37d9fbf5a
Alokananda Y <plantsfortrees@gmail.com>
2024-04-22 07:56:20 +0530

    cart

--- module-3/mobile-app/shopping-cart/app.js
+++ module-3/mobile-app/shopping-cart/app.js
@@ -1,11 +1,17 @@
 const buttonElement = document.getElementById("add-button");
 const inputElement = document.getElementById("input-field");
 const formElement = document.querySelector("form");
+const ulElement = document.querySelector(".food--items");
 
 const logFoodItem = (event) => {
   event.preventDefault();
   const foodItem = inputElement.value.trim();
-  console.log(foodItem);
+  const liElement = document.createElement("li");
+  // Create a text node
+  const textNode = document.createTextNode(foodItem);
+  liElement.appendChild(textNode);
+  //   Append this to the ul
+  ulElement.appendChild(liElement);
   inputElement.value = "";
 };
 
