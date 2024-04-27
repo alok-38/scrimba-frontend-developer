6438cc1a9c3f3d96cef99e3263fa70c79234fb45
Alokananda Y <plantsfortrees@gmail.com>
2024-04-27 20:37:03 +0530

    Blackjack

--- module-3/blackjack-game/app.js
+++ module-3/blackjack-game/app.js
@@ -4,19 +4,25 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
-
-// 1. Store the message-el paragraph in a variable called messageEl
+let messageEl = document.getElementById("message-el")
+// 2. Store the sum paragraph in a variable called sumEl
+const sumEl = document.querySelector('.sum--paragraph');
 
 function startGame() {
-    if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
-    } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game! 😭"
-        isAlive = false
-    }
-    // 2. Display the message in the messageEl using messageEl.textContent
-    console.log(message)
+	// 3. Render the sum on the page using this format -> "Sum: 14"
+	if (sum <= 20) {
+		message = "Do you want to draw a new card?"
+	} else if (sum === 21) {
+		message = "You've got Blackjack!"
+		hasBlackJack = true
+	} else {
+		message = "You're out of the game!"
+		isAlive = false
+	}
+	messageEl.textContent = message
+	sumEl.textContent += sum;
 }
+
+const buttonEl = document.querySelector('button');
+
+buttonEl.addEventListener('click', startGame);
