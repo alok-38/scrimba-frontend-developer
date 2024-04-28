c5625c7220ebc8080aa3a1d8f21a0d72e7c18a78
Alokananda Y <plantsfortrees@gmail.com>
2024-04-28 14:25:50 +0530

    Display the sum

--- module-3/blackjack-game/app.js
+++ module-3/blackjack-game/app.js
@@ -7,15 +7,19 @@ let message = ""
 
 // 1. Store the message-el paragraph in a variable called messageEl
 const messageEl = document.getElementById('message-el');
+// 2. Store the sum paragraph in a variable called sumEl
+const sumEl = document.querySelector('.sum--paragraph');
 
 const startGame = () => {
+	// 3. Render the sum on the page using this format -> "Sum: 14"
+	sumEl.textContent += sum;
 	if (sum <= 20) {
-		message = "Do you want to draw a new card? 🙂"
+		message = "Do you want to draw a new card? "
 	} else if (sum === 21) {
-		message = "Wohoo! You've got Blackjack! 🥳"
+		message = "Wohoo! You've got Blackjack! "
 		hasBlackJack = true
 	} else {
-		message = "You're out of the game! 😭"
+		message = "You're out of the game! "
 		isAlive = false
 	}
 	// 2. Display the message in the messageEl using messageEl.textContent
