0916e232ace106e69c8daf10f412d8fe30cb003b
Alokananda Y <plantsfortrees@gmail.com>
2024-04-28 14:52:36 +0530

    Add to the sum when newCard is clicked

--- module-3/blackjack-game/app.js
+++ module-3/blackjack-game/app.js
@@ -9,9 +9,12 @@ let message = ""
 const messageEl = document.getElementById('message-el');
 // 2. Store the sum paragraph in a variable called sumEl
 const sumEl = document.querySelector('.sum--paragraph');
+// 2. Store the cards paragraph in a variable called cardsEl
+const cardsEl = document.querySelector('.cards--paragraph');
 
 const startGame = () => {
-	// 3. Render the sum on the page using this format -> "Sum: 14"
+	// 3. Render the cars on the page using this format -> "Cards: 10 4"
+	cardsEl.textContent += `${firstCard} ${secondCard}`;
 	sumEl.textContent += sum;
 	if (sum <= 20) {
 		message = "Do you want to draw a new card? "
@@ -27,6 +30,20 @@ const startGame = () => {
 	console.log(message)
 }
 
-const buttonEl = document.querySelector('button');
+// const buttonEl = document.querySelector('button');
 
-buttonEl.addEventListener('click', startGame);
+// buttonEl.addEventListener('click', startGame);
+
+// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+const newCard = () => {
+	console.log(`Drawing a new card from the deck!`);
+	// 1. Create a card variable, and hard code its value to a number (2-11)
+	let card = 5;
+	// 2. Add the new card to the sum variable
+	sum += card;
+	// 3. Call startGame()
+	startGame();
+}
+
+const newCardButtonEl = document.querySelector('.new__card--btn');
+newCardButtonEl.addEventListener('click', newCard);
