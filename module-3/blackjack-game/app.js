8880b82b4c58d11455851004fd931c23d95d9405
Alokananda Y <plantsfortrees@gmail.com>
2024-04-29 06:59:59 +0530

    new card

--- module-3/blackjack-game/app.js
+++ module-3/blackjack-game/app.js
@@ -1,38 +1,43 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
+let firstCard = 10;
+let secondCard = 4;
+let cards = [firstCard, secondCard];
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let cardsParaEl = document.querySelector('.cards--paragraph');
+let sumParaEl = document.querySelector('.sum--paragraph');
 
-function renderGame() {
-	cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
-	sumEl.textContent = "Sum: " + sum
-	if (sum <= 20) {
-		message = "Do you want to draw a new card?"
-	} else if (sum === 21) {
-		message = "You've got Blackjack!"
-		hasBlackJack = true
-	} else {
-		message = "You're out of the game!"
-		isAlive = false
-	}
-	messageEl.textContent = message
+const startGameButton = document.querySelector('.start__card--btn');
+const newCardButton = document.querySelector('.new__card--btn');
+
+const startGame = () => {
+ renderGame();
 }
 
-// 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
+const renderGame = () => {
+	cardsParaEl.textContent = "Cards: " + cards[0] + " " + cards[1];
+	sumParaEl.textContent = "Sum: " + sum;
+	if (sum <= 20) {
+		message = `Do you want to draw a new card?`;
+	} else if (sum === 21) {
+		message = `You've got Blackjack!`
+		hasBlackJack = true;
+	} else {
+		message = `You are out of the game.`;
+		isAlive = false;
+	}
+	messageEl.textContent = message;
+}
+
 const newCard = () => {
-	console.log(`Drawing a new card from the deck!`);
-	// 1. Create a card variable, and hard code its value to a number (2-11)
-	const card = 7;
-	// 2. Add the new card to the sum variable
+	let card = 6;
 	sum += card;
-	// 3. Call startGame()
+	// Push the card to the cards array
+	cards.push(card);
 	renderGame();
 }
 
-const newCardButtonEl = document.querySelector('.new__card--btn');
-newCardButtonEl.addEventListener('click', newCard);
+startGameButton.addEventListener('click', startGame);
+newCardButton.addEventListener('click', newCard);
