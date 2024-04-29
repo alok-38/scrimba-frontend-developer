de68400e73ef63626021b177ded7dee2ac0961d2
Alokananda Y <plantsfortrees@gmail.com>
2024-04-29 06:42:33 +0530

    render game

--- module-3/blackjack-game/app.js
+++ module-3/blackjack-game/app.js
@@ -4,46 +4,30 @@ let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-// Create a new function called startGame() that calls renderGame()
-const startGame = () => {
-	renderGame();
-}
-
-// 1. Store the message-el paragraph in a variable called messageEl
-const messageEl = document.getElementById('message-el');
-// 2. Store the sum paragraph in a variable called sumEl
-const sumEl = document.querySelector('.sum--paragraph');
-// 2. Store the cards paragraph in a variable called cardsEl
-const cardsEl = document.querySelector('.cards--paragraph');
-
-const renderGame = () => {
-	// 3. Render the cars on the page using this format -> "Cards: 10 4"
-	cardsEl.textContent += `${firstCard} ${secondCard}`;
-	sumEl.textContent += sum;
+function renderGame() {
+	cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+	sumEl.textContent = "Sum: " + sum
 	if (sum <= 20) {
-		message = "Do you want to draw a new card? "
+		message = "Do you want to draw a new card?"
 	} else if (sum === 21) {
-		message = "You've got Blackjack! "
+		message = "You've got Blackjack!"
 		hasBlackJack = true
 	} else {
-		message = "You're out of the game! "
+		message = "You're out of the game!"
 		isAlive = false
 	}
-	// 2. Display the message in the messageEl using messageEl.textContent
-	messageEl.textContent = message;
-	console.log(message)
+	messageEl.textContent = message
 }
-
-const buttonEl = document.querySelector('button');
-
-buttonEl.addEventListener('click', startGame);
 
 // 2. Create a function newCard() that logs out "Drawing a new card from the deck!"
 const newCard = () => {
 	console.log(`Drawing a new card from the deck!`);
 	// 1. Create a card variable, and hard code its value to a number (2-11)
-	let card = 6;
+	const card = 7;
 	// 2. Add the new card to the sum variable
 	sum += card;
 	// 3. Call startGame()
