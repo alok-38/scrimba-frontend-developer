22a9d5fa079e8f4e048fdce8a53638e5edf428d0
Alokananda Y <plantsfortrees@gmail.com>
2024-05-10 08:58:39 +0530

    Realtime Database

--- module-3/mobile-app/reel-time/app.js
+++ module-3/mobile-app/reel-time/app.js
@@ -1,17 +1,31 @@
-import { add } from "./functions.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+// Challenge: Import the 'getDatabase' from
+// "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+const appSettings = {
+  databaseURL:
+    "https://playground-c0cdd-default-rtdb.asia-southeast1.firebasedatabase.app",
+};
+
+const app = initializeApp(appSettings);
+
+const database = getDatabase(app);
+
+const moviesInDB = ref(database, "movies");
 
 const buttonEl = document.querySelector("#add-button");
 const inputEl = document.querySelector("#input-field");
 
-const useFunctions = () => {
-  console.log("Button clicked");
-  const inputValue = parseFloat(inputEl.value);
-  if (!isNaN(inputValue)) {
-    const result = add(inputValue, 10);
-    console.log("Result:", result);
-  } else {
-    console.log("Invalid input. Please enter a valid number.");
-  }
+const acceptMovieInput = () => {
+  let movieInput = inputEl.value.trim();
+  push(moviesInDB, movieInput);
+  console.log(`${movieInput} added to database`);
+  inputEl.value = "";
 };
 
-buttonEl.addEventListener("click", useFunctions);
+buttonEl.addEventListener("click", acceptMovieInput);
