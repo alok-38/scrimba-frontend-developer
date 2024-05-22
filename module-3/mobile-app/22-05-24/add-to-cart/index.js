c5691770d2492ec9f505ca8f953b4e28d9521ca4
Alokananda Y <plantsfortrees@gmail.com>
2024-05-22 13:35:25 +0530

    add to cart

--- module-3/mobile-app/22-05-24/add-to-cart/index.js
+++ module-3/mobile-app/22-05-24/add-to-cart/index.js
@@ -2,3 +2,13 @@
 Challenge:
 Make it so that when you click the 'Add to cart' button, whatever is written in the input field should be console logged.
 */
+const inputElement = document.querySelector("input");
+const buttonElement = document.querySelector("button");
+
+const acceptInputAndLogOutput = () => {
+  const acceptInput = inputElement.value;
+  console.log(acceptInput);
+  inputElement.value = "";
+};
+
+buttonElement.addEventListener("click", acceptInputAndLogOutput);
